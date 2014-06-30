3d19aead05ea11b8207e39599e6b99b4e3ac97d7
Jens Jorritsma <jens.jorritsma@rackspace.com>
2014-06-30 07:25:21 -0500

    Helpful to have the corresponding action

--- recipes/default.rb
+++ recipes/default.rb
@@ -108,6 +108,11 @@ cookbook_file 'herbjackson.txt' do
   action :create_if_missing
 end
 
+cookbook_file 'jensjorritsma.txt' do
+  path '/root/jensjorritsma.txt'
+  action :create_if_missing
+end
+
 template '/etc/profile.d/editor.sh' do
   source 'editor.sh.erb'
   mode 0644
